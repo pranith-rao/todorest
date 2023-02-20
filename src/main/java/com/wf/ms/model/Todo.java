5fd436eaa3808405121beaecd5d34bbed28e0560
Pranith Rao <pranithrao3@gmail.com>
2023-02-20 13:28:06 +0530

    removed code smells

--- src/main/java/com/wf/ms/model/Todo.java
+++ src/main/java/com/wf/ms/model/Todo.java
@@ -72,6 +72,11 @@ public class Todo {
 	
 	public Todo() {
 		// TODO
+		id = 1;
+		user = "Anil";
+		des = "Testing now";
+		targetDate = new Date();
+		done = false;
 	}
 
 	public Todo(int id, String user, String des, Date targetDate, boolean done) {
